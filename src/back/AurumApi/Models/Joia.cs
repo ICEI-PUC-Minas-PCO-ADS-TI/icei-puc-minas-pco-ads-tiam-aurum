79a296c2972fc2b944867f4bdf614cc3c0d6d9c7
Guilherme Campos <guilhermealc01@gmail.com>
2025-05-19 21:26:04 -0300

    Adiciona nome para Joia

--- src/back/AurumApi/Models/Joia.cs
+++ src/back/AurumApi/Models/Joia.cs
@@ -7,6 +7,8 @@ namespace AurumApi.Models
     {
         [Column("id")]
         public int Id { get; set; }
+        [Column("nome")]
+        public string Nome { get; set; }
         [Column("descricao")]
         public string? Descricao { get; set; }
         [Column("preco")]
