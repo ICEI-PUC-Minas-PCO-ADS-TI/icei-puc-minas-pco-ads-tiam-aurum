133d6da9497fa8a4d6c5941869dc7627020387b7
Rafael Brandao <rbs13bg13@gmail.com>
2025-06-12 17:08:46 -0300

    adicionado tarefa

---
+++ src/back/AurumApi/Models/Tarefa.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AurumApi.Models
+{
+    [Table("tarefa")]
+    public class Tarefa
+    {
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("descricao")]
+        public string? Descricao { get; set; }
+
+        [Column("data_criado")]
+        public DateTime? DataCriado { get; set; }
+
+        [Column("usuario_id")]
+        public int UsuarioId { get; set; }
+
+        public Usuario? Usuario { get; set; }
+        [Column("data_a_realizar")]
+        public DateTime? DataRealizar { get; set; }
+    }
+}
